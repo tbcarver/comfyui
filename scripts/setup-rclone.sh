85284a0ae62262f6b69356708f8b4e386fe33c07
Tyler Carver <tbcarver@yahoo.com>
2024-09-16 17:56:41 +0300

    Opening nano for the rclone.conf

--- scripts/setup-rclone.sh
+++ scripts/setup-rclone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir ~/.config/rclone
 echo "open home existing ~/.config/rclone/rclone.conf"
-echo "nano ~/.config/rclone/rclone.conf"
 echo "paste in contents and save"
+nano ~/.config/rclone/rclone.conf
